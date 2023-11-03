2c45279bcbbc3508a8f990af10b5de7a1a436ae8
ArthurSZANTYR <szantyrarthur.pto@gmail.com>
2023-11-03 14:02:49 +0100

    precision test : 78.52%

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import os
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
+from torch.utils.data import DataLoader
 
 import warnings  
 warnings.filterwarnings('ignore')  #pour enlever les warning message
@@ -10,7 +11,7 @@ warnings.filterwarnings('ignore')  #pour enlever les warning message
 from glob import glob #pour parcourir dossier
 
 # Import your custom model class
-from train import Model
+from train import Model, LeafDataset
 
 # Define the same transformation as in your training script
 transform = A.Compose([
@@ -21,10 +22,10 @@ transform = A.Compose([
             ], p=1.0)
 
 # Define a function to perform inference
-def predict(model, image_path, transform):
+def predict(model, image, transform):
     # Load and preprocess the image
-    image = plt.imread(image_path)
-    image = transform(image=image)['image']
+    #image = plt.imread(image_path)
+    #les images sont déja ransformé avec l'appel de LeafDataset
     image = image.unsqueeze(0)  # Add a batch dimension
 
     # Make a prediction
@@ -39,19 +40,44 @@ def predict(model, image_path, transform):
 
 
 if __name__ == '__main__':
+
+    test_set = LeafDataset()
+
     # Load your trained model
     model_path = 'leaf_cnn_mlp.pt'
     model = Model()
     model.load_state_dict(torch.load(model_path))
     model.eval()  # Set the model to evaluation mode
 
-    # Example inference on an image
-    image_folder = 'dataset/plant-pathology-2020-fgvc7/images'
-    image_files = glob(os.path.join(image_folder, '*.jpg'))  #pour creer un objet pour parcourir toutes les images "fichier qui termine par jpg"
+    correct_predictions = 0
+    total_predictions = 0
 
-    for image_file in image_files:
-        file_name = os.path.basename(image_file) 
+    # Example inference on the test images
 
-        probabilities, predicted_class = predict(model, image_file, transform)
-        print(f'{file_name} : {predicted_class}')
+    for index in range(len(test_set)-1):
+        image, label = test_set[index]
+
+        _, predicted_class = predict(model, image, transform)
+        true_class = label.item()
+
+
+        if true_class == predicted_class.item():
+            correct_predictions += 1
+        
+        total_predictions += 1
+        print(total_predictions)  #pour suivre l'évolution
+
+    accuracy = correct_predictions / total_predictions
+    print(f'Accuracy: {accuracy * 100:.2f}%')
+
+
+    ## Example inference on an image
+    #image_folder = 'dataset/plant-pathology-2020-fgvc7/images'
+    #image_files = glob(os.path.join(image_folder, 'Train*'))  #pour creer un objet pour parcourir toutes les images "fichier qui termine par jpg"
+#
+    #for image_file in image_files:
+    #    file_name = os.path.basename(image_file) 
+#
+    #    probabilities, predicted_class = predict(model, image_file, transform)
+    #    print(f'{file_name} : {predicted_class}')
    
